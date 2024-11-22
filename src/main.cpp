39d157f87027dca12244e7fb4a2f563f9ebd874a
hyetae <favor@sookmyung.ac.kr>
2024-11-22 10:55:31 +0900

    chore: fix typo

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 #include "tcpHandler.hpp"
 #include "encodeHandler.hpp"
 #include "objectHandler.hpp"
-
-#include "data.hpp"
 #include "cipherHandler.hpp"
 
 #include <unistd.h>
@@ -14,17 +12,16 @@
 
 int main()
 {
-    const int width     = 1280;
-    const int height    = 720;
-    const int fps       = 15;
-    const int bitrate   = 1000000;
+    const int width = 1280;
+    const int height = 720;
+    const int fps = 30;
+    const int bitrate = 1000000;
 
     // Get current working directory
     char buf[PATH_MAX];
     getcwd(buf, PATH_MAX);
     std::string path(buf);
 
-    Utils utils;
     CaptureHandler capHandler;
     TcpHandler tcpHandler;
     ObjectHandler objHandler;
@@ -47,34 +44,24 @@ int main()
             continue;
         }
 
-        std::string timestamp = data::GetCurrentTime();
-        std::cout << "===== timestamp: " << timestamp << " =====" << std::endl;
-
         // TODO: 전처리
-        
+
         // 모델 추론
-        object::Detection detections;
-        detections = objHandler.DetectObject(inFrame);
-
-
-        // 디버깅용 화면 출력
-        capHandler.ShowFrame(inFrame, detections);
-
+        object::Detection detections = objHandler.DetectObject(inFrame);
 
         // TODO: 결과 파싱, json화, 전송
         nlohmann::json json = objHandler.CreateJson(detections);
-
+        
         // h.264 압축
         encodeHandler.EncodeFrame(inFrame, encodedFrame);
         
         // 암호화 && tcp 전송
-        auto key = cipherHandler.Init();
-
+        // auto key = cipherHandler.Init();
         // tcpHandler.SendData(key, (size_t)32UL); 
 
         // cipherHandler.EncryptData(encodedFrame, sizeof(encodedFrame), encryptedFrame, decryptedFrame);
-        auto iv = cipherHandler.EncryptData(encodedFrame, sizeof(encodedFrame), encryptedFrame);
-        tcpHandler.SendData(iv, (size_t)12UL); 
+        // auto iv = cipherHandler.EncryptData(encodedFrame, sizeof(encodedFrame), encryptedFrame);
+        // tcpHandler.SendData(iv, (size_t)12UL); 
 
         // if (cipherHandler.isEqual(encodedFrame, decryptedFrame, sizeof(encodedFrame)))
         // {
@@ -82,6 +69,6 @@ int main()
         // }
 
         // tcp 전송
-        tcpHandler.SendData(encryptedFrame); 
+        // tcpHandler.SendData(encryptedFrame); 
     }
 }
