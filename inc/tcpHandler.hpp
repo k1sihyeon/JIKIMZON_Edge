b6d49000cbfaec017004210d3c22dd7a80437806
k1sihyeon <tlgus530@naver.com>
2024-11-24 20:49:56 +0900

    feat: add SendJSON in TcpHandler

--- inc/tcpHandler.hpp
+++ inc/tcpHandler.hpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
-
+#include <nlohmann/json.hpp>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
@@ -19,8 +19,9 @@ public:
     
     void InitSocket();
     void SendData(const unsigned char* ci, size_t size);
-    void SendData(std::vector<uint8_t>& frame);
-    //void SendHeader(Frame::Header& header);
+    void SendData(std::vector<uint8_t>& data);
+    void SendFrame(frame::Frame& frame);
+    void SendJson(const nlohmann::json& json);
 
 private:
     
--- src/tcpHandler.cpp
+++ src/tcpHandler.cpp
@@ -9,14 +9,14 @@
 
 void TcpHandler::InitSocket()
 {
-  mSockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (mSockfd < 0)
+  	mSockfd = socket(AF_INET, SOCK_STREAM, 0);
+  	if (mSockfd < 0)
 	{
 		std::cerr << "socket: msockfd" << std::endl;
 		exit(EXIT_FAILURE);
 	}
 
-  // Bind socket
+  	// Bind socket
 	mServerAddr.sin_family = AF_INET;
 	mServerAddr.sin_addr.s_addr = INADDR_ANY;
 	mServerAddr.sin_port = htons(PORT);
@@ -48,13 +48,13 @@ void TcpHandler::SendData(const unsigned char* ci, size_t size)
         exit(EXIT_FAILURE);
     }
 
-    if (send(mClientSock, ci, (size_t)12UL, 0) < 0)
+    if (send(mClientSock, ci, size, 0) < 0)
     {
         perror("send iv");
 	}
 }
 
-void TcpHandler::SendData(std::vector<uint8_t>& frame)
+void TcpHandler::SendData(std::vector<uint8_t>& data)
 {
     if (mClientSock < 0)
     {
@@ -62,8 +62,28 @@ void TcpHandler::SendData(std::vector<uint8_t>& frame)
         exit(EXIT_FAILURE);
     }
 
-	if (send(mClientSock, frame.data(), frame.size(), 0) < 0)
+	if (send(mClientSock, data.data(), data.size(), 0) < 0)
     {
-        perror("send frame");
+        perror("send data");
     }
+}
+
+void TcpHandler::SendFrame(frame::Frame& frame)
+{
+	std::vector<uint8_t> buffer = frame.Serialize();
+	this->SendData(buffer);
+}
+
+void TcpHandler::SendJson(const nlohmann::json& json)
+{
+	if (mClientSock < 0)
+    {
+        std::cerr << "not valid client sock" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+	if (send(mClientSock, json.dump().c_str(), json.dump().size(), 0) < 0)
+	{
+		perror("send json");
+	}
 }
