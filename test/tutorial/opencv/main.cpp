d009465a316816cc871cfa3821d65c9ed4bfbcb6
hyetae <favor@sookmyung.ac.kr>
2024-11-26 12:08:23 +0900

    fix: test for preprocess

--- test/tutorial/opencv/main.cpp
+++ test/tutorial/opencv/main.cpp
@@ -1,18 +1,42 @@
 #include "preprocessHandler.hpp"
 
+#include <iostream>
+
 int main()
 {
-    cv::Mat input = cv::imread("./lenna.jpg");
+    cv::Mat input;
+    cv::Mat output;
+
+    input = cv::imread("./lenna.jpg");
     if (input.empty())
     {
         std::cerr << "Error: imread" << std::endl;
     }
 
-    cv::Mat output;
-    adaptiveExposureControl(input, output);
-
     cv::imshow("Original", input);
-    cv::imshow("Adaptive Exposure Control", output);
+
+    // std::cout << "input: " << input.rows << ", " << input.cols << std::endl;
+
+    PreprocessHandler* ph = new PreprocessHandler(input, output);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    ph->Exposure(input.rows, input.cols);
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    std::cout << "norm: " << duration << "ms" << std::endl;
+
+    cv::imshow("norm", output);
+    output.release();
+
+    start = std::chrono::high_resolution_clock::now();
+    ph->Threading();
+    end = std::chrono::high_resolution_clock::now();
+
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    std::cout << "thre: " << duration << "ms" << std::endl;
+
+    cv::imshow("thre", output);
     cv::waitKey(0);
 
     return 0;
